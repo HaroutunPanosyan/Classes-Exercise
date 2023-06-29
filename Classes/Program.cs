f55ff8bfcf89be79f47cbe9f609bf495c39df49c
Haroutun Panosyan <harupanosyan@gmail.com>
2023-06-29 09:11:27 -0700

    Created the class, properties and set them.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+
+            car1.Make = "Ford";
+            car1.Model = "F-150";
+            car1.Year = 2023;
+
+            Console.WriteLine($"The car is a {car1.Make} {car1.Model} made in {car1.Year}.");
         }
     }
 }
